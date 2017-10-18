8c90013586ab0e96be29ddf61f8095e89b768722
def00111 <def00111@users.noreply.github.com>
2017-10-18 21:42:33 +0200

    updated add-on

--- background.js
+++ background.js
@@ -1,22 +1,35 @@
 "use strict";
-const {contextMenus, i18n} = browser;
+const {contextMenus, i18n, tabs, notifications} = browser;
 
 const _ = i18n.getMessage;
 
-let input = null;
+function notify(message) {
+  notifications.create("copy-link-text-notification", {
+    type: "basic",
+    title: "Copy Link Text",
+    message
+  });
+}
+
+let doCopy = String(str => {
+  document.addEventListener("copy", evt => {
+    evt.clipboardData.setData("text", str);
+    evt.preventDefault();
+  }, {
+    capture: true,
+    once: true
+  });
+  document.execCommand("copy");
+});
 
 contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId == "copy-link-text") {
-    if (!input) {
-      input = document.createElement("input");
-      document.body.append(input);
-    }
-
-    if (input.value != info.linkText) {
-      input.value = info.linkText;
-      input.select();
-    }
-    document.execCommand("copy");
+    tabs.executeScript(tab.id, {
+      code: `(${doCopy})("${info.linkText}");`
+    }).catch(error => {
+      console.error(error)
+      notify("Failed to copy the text.");
+    });
   }
 });
 
