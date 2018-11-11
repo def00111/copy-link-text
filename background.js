b3a2bccf51f24fdbce03287005a4258951e6f01c
def00111 <def00111@users.noreply.github.com>
2018-11-11 14:48:21 +0100

    Add files via upload

--- background.js
+++ background.js
@@ -1,18 +1,10 @@
 "use strict";
 
-function notify(message) {
-  browser.notifications.create("copy-link-text-notification", {
-    type: "basic",
-    title: "Copy Link Text",
-    message
-  });
-}
-
 browser.menus.onClicked.addListener((info, tab) => {
   if (info.menuItemId == "copy-link-text") {
     let linkText = JSON.stringify(info.linkText).slice(1, -1);
     navigator.clipboard.writeText(linkText).catch(() => {
-      notify("Failed to copy the link text.");
+      console.error("Failed to copy the link text.");
     });
   }
 });
