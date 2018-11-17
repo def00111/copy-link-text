01c8186463708e7f2dd4987ad19071ecfe1e4224
def00111 <def00111@users.noreply.github.com>
2018-11-17 20:07:36 +0100

    Add files via upload

--- background.js
+++ background.js
@@ -1,12 +1,62 @@
 "use strict";
 
-browser.menus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId == "copy-link-text") {
-    let linkText = JSON.stringify(info.linkText).slice(1, -1);
-    navigator.clipboard.writeText(linkText).catch(() => {
-      console.error("Failed to copy the link text.");
-    });
+browser.menus.onClicked.addListener(async (info, tab) => {
+  if (info.menuItemId != "copy-link-text") {
+    return;
   }
+
+  let text = info.linkText;
+  if (info.modifiers &&
+      info.modifiers.length == 1 &&
+      info.modifiers[0] == "Shift") {
+    let result;
+    try {
+      result = await browser.tabs.executeScript(tab.id, {
+        frameId: info.frameId,
+        code: `
+          var text = "";
+          var elem = browser.menus.getTargetElement(${info.targetElementId});
+          while (elem) {
+            var url = elem.href;
+            if (url) {
+              // Handle SVG links:
+              if (typeof url == "object" && url.animVal) {
+                url = new URL(url.animVal, elem.baseURI).href;
+              }
+            }
+
+            if (!url) {
+              var href = elem.getAttribute("href") ||
+                         elem.getAttributeNS("http://www.w3.org/1999/xlink", "href");
+
+              if (href && /\S/.test(href)) {
+                url = new URL(href, elem.baseURI).href;
+              }
+            }
+
+            if (url && url == "${info.linkUrl}") {
+              if (elem.hasAttribute("title")) {
+                text = elem.getAttribute("title");
+              }
+              break;
+            }
+            elem = elem.parentElement;
+          }
+          text;
+        `,
+      });
+    }
+    catch(ex) {
+    }
+    if (result && result[0] != "") {
+      text = result[0];
+    }
+  }
+
+  text = JSON.stringify(text).slice(1, -1);
+  navigator.clipboard.writeText(text).catch(() => {
+    console.error("Failed to copy the link text.");
+  });
 });
 
 browser.menus.create({
